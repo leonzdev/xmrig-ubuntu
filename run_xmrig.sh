203eb6f8699c449952e5845288d005ecae00ca44
leonzdev <leonz.dev@gmail.com>
2017-09-20 05:04:50 +0000

    Add environment variable check and THREADS for thread count

--- run_xmrig.sh
+++ run_xmrig.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env sh
-./xmrig -o $URL -u $WALLET_ADDRESS -p $PASSWORD
+if [ "x$URL" = 'x' ];
+then
+    echo "URL is not defined or is empty"
+    exit 1
+fi
+if [ "x$WALLET_ADDRESS" = 'x' ];
+then
+    echo "WALLET_ADDRESS is not defined or is empty"
+    exit 1
+fi
+
+PASSWORD=${PASSWORD:-x}
+THREADS=${THREADS:-1}
+./xmrig -o $URL -u $WALLET_ADDRESS -p $PASSWORD -t $THREADS --donate-level=1 --no-color
