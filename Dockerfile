377555e36a012c75c7c76c9218a12dfeba8162bc
leonzdev <leonz.dev@gmail.com>
2017-10-22 04:53:38 +0000

    Add signal handler form SIGTERM

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,9 @@ COPY run.sh /
 RUN adduser --system --disabled-password --home /ubuntu ubuntu && \
     apt-get update -y && \
     apt-get install -y wget libcurl4-openssl-dev libuv1 && \
-    wget -O /app https://github.com/leonzdev/xmrig/releases/download/leonzdev_0.0.2/xmrig && \
+    wget -O /app https://github.com/leonzdev/xmrig/releases/download/leonzdev_0.0.3/xmrig && \
     chmod a+x /app && \
     chmod a+x /run.sh
 USER ubuntu 
-ENTRYPOINT ["/bin/sh"]
-CMD ["/run.sh"]
+ENTRYPOINT ["/run.sh"]
 
--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env sh
+set -vx
+
+term_handler() {
+    if [ $PID -ne 0 ]; then
+        echo "SIGTERM; passthrough to pid=$PID"
+        kill -TERM "$PID"
+        wait "$PID"
+    fi
+    exit 143;
+}
+
 if [ "x$URL" = 'x' ];
 then
     echo "URL is not defined or is empty"
@@ -10,8 +21,21 @@ then
     exit 1
 fi
 
+PID=0
 PASSWORD=${PASSWORD:-x}
 THREADS=${THREADS:-1}
 THROTTLE=${THROTTLE:-101}
-./app -o $URL -u $WALLET_ADDRESS -p $PASSWORD -t $THREADS -v 2 --donate-level=1 --no-color --throttle=$THROTTLE
+VARIANT=${VARIANT:-4}
+
+trap 'kill ${!}; term_handler' TERM
+
+./app -o $URL -u $WALLET_ADDRESS -p $PASSWORD -t $THREADS --av=$VARIANT --donate-level=1 --no-color --throttle=$THROTTLE --background
+
+PID=$(pidof app)
+
+while true
+do
+    tail -f /dev/null & wait ${!}
+done
+
 
